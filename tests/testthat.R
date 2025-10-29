ed09d24699580b80cf8053679a883580215d26ee
Dongdong Kong <kongdd@users.noreply.github.com>
2025-10-29 14:07:28 +0800

    tests起步

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,49 +6,32 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
+fprintf <- function(...) cat(sprintf(...))
+
 if (Sys.getenv("GITHUB_ACTIONS") == "true") {
-  print("GitHub Actions 环境下执行认证")
+
+  fprintf("RETICULATE_PYTHON: %s\n", Sys.getenv("RETICULATE_PYTHON"))
+  
+  Sys.setenv(RETICULATE_PYTHON = Sys.which("python")) # Set Python interpreter
+  reticulate::use_python(Sys.which("python"))         # double confirm
 }
-
-
-Sys.getenv("RETICULATE_PYTHON")
-Sys.setenv(RETICULATE_PYTHON = Sys.which("python"))
-
-reticulate::use_python(Sys.which("python"))
-
-
-tryCatch({
-  print(Sys.which("python"))
-  print(Sys.which("python3"))
-
-  Sys.setenv(RETICULATE_PYTHON = Sys.which("python"))
-  # print(python)
-}, error = function(e) {
-  message(sprintf('%s', e$message))
-})
-
 
 library(testthat)
 library(rgee2)
 
-# test_check("rgee2")
-test_that("auth works", {
+auth <- function() {
   # token <- paste(readLines("token.json", encoding = "UTF-8"), collapse = "")
   tryCatch({
-    reticulate::py_config()
-  }, error = function(e) {
-    message(sprintf('%s', e$message))
-  })
-
-
-  tryCatch({
     # use_backend()
-    # Sys.setenv(RETICULATE_PYTHON = python)
     print(system("pip list earthengine-api | grep earthengine-api"))
     token <- Sys.getenv("EARTHENGINE_TOKEN")
     ee_auth_ci(token)
   }, error = function(e) {
     message(sprintf("%s", e$message))
   })
-  
+}
+
+# test_check("rgee2")
+test_that("auth works", {
+  expect_no_error(auth())
 })
